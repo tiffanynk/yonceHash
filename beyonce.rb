a7f5cb090ddc8a5401ee2dae2091e5983a131c8e
DamonLC21 <chiversdamon@gmail.com>
2020-09-17 10:30:37 -0600

    initial commit

---
+++ beyonce.rb
@@ -0,0 +1,23 @@
+require 'pry'
+require_relative './hash.rb'
+
+#    puts all the songs
+#    puts all the movies
+#    create an array with a list of all of Beyonce's hit songs 
+#    create an array with a list of all of Beyonce's fierceness ratings
+#    give me all the songs where Beyonce is wearing a bodysuit or a bodysuit is part of the video theme
+#    create an array with all of the songs where Beyonce's fierceness is greater than or equal to 8
+#    create an array with all of the movies Beyonce made in 2006 and after
+#    find all the hit songs where beyonce was in a group
+#    find a hit song where Beyonce's hair is blonde
+#    find the hit song sorry
+#    find all hit songs where Beyonce's fierceness rating is 10
+#    sum up Beyonces fierceness value for all of her hit songs
+#    get the average fierceness value for all of her hit songs
+#    sum up Beyonces rating value for all of her movies
+#    get the average rating value for all of her movies
+#    sum up the total number of dancers in all of the hit song videos
+#    create an array of beyonces hairstyles without repeats
+#    create an object where the properties are song names and the value is an object which contains that fierceness and average fierceness
+#    create an object where the properties are movie names and the value is an object which contains that rating and average rating
+#    create a hash with beyonces hairstyles with a tally of each hairstyle ie { 'blonde': 3, ect.}
